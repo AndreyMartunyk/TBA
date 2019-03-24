3191e1b0b70d639898f1905bcf126fb2e21a647e
Andrey Martunyk <aniks.zoom@gmail.com>
2019-03-24 03:41:18 +0200

    some small changes

--- TheBestAdventure/LessThenZeroPointException.cs
+++ TheBestAdventure/LessThenZeroPointException.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TheBestAdventure
 {
-    class LessThenZeroPointException
+    class LessThenZeroPointException : Exception
     {
+
+        public LessThenZeroPointException(string message = "Одна из координат точки меньше 0!!!")
+            : base(message)
+        {
+        }
+
+        public LessThenZeroPointException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected LessThenZeroPointException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
     }
 }
--- TheBestAdventure/Point2D.cs
+++ TheBestAdventure/Point2D.cs
@@ -6,7 +6,75 @@ using System.Threading.Tasks;
 
 namespace TheBestAdventure
 {
-    class Point2D
+    class Point2D : IComparable
     {
+        public int X
+        {
+            get
+            {
+                return _x;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new LessThenZeroPointException();
+                }
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return _y;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new LessThenZeroPointException();
+                }
+            }
+        }
+
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point2D(Point2D p)
+        {
+            _x = p._x;
+            _y = p._y;
+        }
+
+        public int CompareTo(object obj)
+        {
+            int res = 1;
+
+            Point2D p = (Point2D)obj;
+
+            if (IsEqual(p))
+            {
+                res = 0;
+            }
+            else if ((p.X + p.Y) < (X + Y))
+            {
+                res = -1;
+            }
+
+            return res;
+        }
+
+        public bool IsEqual(Point2D p)
+        {
+            return (p.X == X) && (p.Y == Y);
+        }
+
+        private int _x;
+        private int _y;
+
     }
 }
