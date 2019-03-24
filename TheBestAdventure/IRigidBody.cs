d7e8c20a5b3acef410722657bfc949ce1ce2dcd2
Andrey Martunyk <aniks.zoom@gmail.com>
2019-03-24 03:40:48 +0200

    added IRigidBody interface
    интерфейс "твердого тела", то-есть те обькты на которых будут действовать коллизия

---
+++ TheBestAdventure/IRigidBody.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestAdventure
+{
+    interface IRigidBody
+    {
+        Point2D start { get;  set; }
+        Point2D end { get;  set; }
+
+        Point2D[] GetBoxCollider()
+        {
+            return new Point2D(1, 1);
+        }
+    }
+}
